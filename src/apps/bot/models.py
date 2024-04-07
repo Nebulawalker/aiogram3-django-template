34e92eb57ebc4f75d32f727bfc580c0536cd18cf
Dmytro Hryshchenko <zzzxxxxx28540@gmail.com>
2024-04-07 23:40:05 +0100

    Added new model for User Logs called UserLogs, added field to User model to integrate user logs. Adapted fields, new full name function.

--- src/apps/bot/models.py
+++ src/apps/bot/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MinLengthValidator
+from django.core.validators import MinLengthValidator, RegexValidator
 
 from utils.validators.time import only_past_datetime
 
@@ -15,8 +15,28 @@ class User(models.Model):
     registration_date = models.DateTimeField(auto_now_add=True, validators=[only_past_datetime])
     last_activity = models.DateTimeField(auto_now=True)
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}" if self.last_name else self.first_name
+
     def save(self, *args, **kwargs):
+        self.first_name = self.first_name.capitalize()
+
+        if self.last_name is not None:
+            self.last_name = self.last_name.capitalize()
+
         if self.is_premium is None:
             self.is_premium = False
 
         super(User, self).save(*args, **kwargs)
+
+
+class UserLogs(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=False)
+    message_body = models.CharField(max_length=4096)
+    message_sent_datetime = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.message_body is None:
+            self.message_body = 'Is not text.'
+
+        super(UserLogs, self).save(*args, **kwargs)
