2278cf3c7224cd51081848517604bc7970cea844
Dmytro Hryshchenko <zzzxxxxx28540@gmail.com>
2024-04-13 00:01:04 +0100

    Updated main urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views import View
 
 urlpatterns = [
+    path('', View.as_view(), name='main-page'),
+    path('bot-admin/', include(('apps.bot.urls', 'bot-admin'))),
     path('admin/', admin.site.urls),
 ]
