ec846c3548cc0496bee5fa0c413a5549c52d040e
Dmytro Hryshchenko <zzzxxxxx28540@gmail.com>
2024-04-13 00:00:31 +0100

    Other handlers adapted for middleware

--- src/apps/bot/bot/handlers/other.py
+++ src/apps/bot/bot/handlers/other.py
@@ -1,4 +1,5 @@
 from aiogram import types, Router
+from aiogram.types import Message, CallbackQuery
 
 from apps.bot.bot.middleware.collect_data import CollectData, CollectCallbackData
 
@@ -6,3 +7,13 @@ other_router = Router()
 
 other_router.message.middleware(CollectData())
 other_router.callback_query.middleware(CollectCallbackData())
+
+
+@other_router.message()
+async def handle_other_messages(message: Message):
+    pass
+
+
+@other_router.callback_query()
+async def handle_other_callback(query: CallbackQuery):
+    pass
